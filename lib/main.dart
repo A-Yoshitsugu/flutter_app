c564306108583672fcf84d542ae0a2a34655a535
a.yoshitsugu <a.yoshitsugu.gude@gmail.com>
2021-10-12 13:41:51 +0900

    MemoApp_1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/next_page.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -7,6 +9,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
+  //コメントアウト
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
@@ -28,6 +31,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<String> titlelist = ['Amazon', 'ZOZO', 'DAZN'];
   int _counter = 0;
 
   void _incrementCounter() {
@@ -40,27 +44,39 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('パスワードポスト'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body:ListView.builder(
+        itemCount: titlelist.length,
+          itemBuilder: (BuildContext context, int i){
+            return Column(
+              children: [
+                ListTile(
+                  onTap: () {
+                    ///画面遷移の処理
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => NextPage(titlelist[i], 'IDを表示', 'パスワードを表示')));
+                  },
+                leading: Icon(Icons.vpn_key),
+                title: Text(titlelist[i]),
+              ),
+                Divider(),
+              ],
+            );
+          },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          ///ボタンが押された時の処理
+          titlelist.add('Google');
+          print(titlelist);
+          setState(() {
+
+          });
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), 
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
